46d4403ffe1357fbe169fd9f1dce1844d73f9f41
anagabyperalta03 <110991307+anagabyperalta03@users.noreply.github.com>
2022-08-31 07:55:03 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    `)
+basic.clearScreen()
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    `)
+basic.clearScreen()
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
